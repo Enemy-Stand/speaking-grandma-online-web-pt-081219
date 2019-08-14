32155aa88114f0493d90e1eab30a402272d1a10e
Joseph Sesti <sestijoey63@gmail.com>
2019-08-14 18:40:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
-    return
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
